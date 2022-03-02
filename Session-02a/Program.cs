0731f4e2334b8ca9e88f1d4d95ac3d46f31c0fd1
Giannis Eskioglou <johneski65@gmail.com>
2022-03-02 15:45:13 +0200

    Solutions to calculation problems

--- Session-02a/Program.cs
+++ Session-02a/Program.cs
@@ -1,2 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1
+Console.WriteLine("What's your name?");
+string name = Console.ReadLine();
+Console.WriteLine("Hello " + name + "!");
+
+//2 
+Console.WriteLine("Give two numbers separated with space:");
+while (true)
+{
+    try
+    {
+        List<float> numbers = Console.ReadLine().Trim().Split().Select(s => s.Replace(".", ",")).Select(s => float.Parse(s)).ToList();
+        Console.WriteLine("Sum: " + (numbers[0] + numbers[1]) + "\n" + "Division: " + (numbers[0] / numbers[1]));
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Wrong numbers please try again...");
+    }
+}
+
+//3
+Console.WriteLine($"{-1 + 5 * 6}");
+Console.WriteLine($"{38 + 5 % 7}");
+Console.WriteLine($"{14 + (-3 * 6 / (double)7)}");
+Console.WriteLine($"{2 + (13 / (double)6) * 6 + Math.Sqrt(7)}");
+Console.WriteLine($"{(Math.Pow(6, 4) + Math.Pow(5, 7)) / (9 % 4)}");
+
+//4
+string[] genders = {"female", "male"};
+while (true)
+{
+    try
+    {
+        Console.Write("Enter your age: ");
+        string inputAge = Console.ReadLine();
+        int age = int.Parse(inputAge);
+        if(age < 10)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        Console.Write("Enter your gender: ");
+        string gender = Console.ReadLine();
+        bool correct = false;
+        foreach(string gen in genders)
+        {
+            if (gender.Trim().ToLower() == gen)
+            {
+                correct = true;
+                break;
+            }
+        }
+        if (!correct) throw new ArgumentException();
+
+        Console.WriteLine($"You are {gender} and look younger than {age}.");
+        break;
+
+    }catch(ArgumentOutOfRangeException outOfRangeEx) 
+    { 
+    
+        Console.WriteLine("You cannot enter less than 10!");
+    }
+    catch (ArgumentException ax)
+    {
+        Console.WriteLine("wrong gender!");
+    }
+
+    catch (Exception ex)
+    {
+        Console.WriteLine("Wrong input!");
+    }
+}
+
+//5
+int seconds;
+while (true)
+{
+    try
+    {
+        Console.Write("Enter an integer: ");
+        string input = Console.ReadLine();
+        seconds = Int32.Parse(input);
+        break;
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine("Please enter a valid integer number");
+    }
+}
+
+//6
+
+
+//7
+TimeSpan time = TimeSpan.FromSeconds(seconds);
+
+Console.WriteLine(time.ToString(@"hh\:mm\:ss\:fff"));
+
+
+

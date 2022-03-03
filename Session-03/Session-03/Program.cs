cc92227480aaee761f0c01270c31f1806b8413f5
Giannis Eskioglou <johneski65@gmail.com>
2022-03-03 16:28:04 +0200

    Delete University file outside of Session-03

--- Session-03/Session-03/Program.cs
+++ Session-03/Session-03/Program.cs
@@ -1,6 +1,2 @@
 ﻿using Session_03;
 
-Person p = new Person("sdf", 30);
-Console.WriteLine(p.GetName());
-Professor pro = new Professor("sdfs", 30, "rank", new Course[] { new Course("fsdf", "fsdf")});
-Console.WriteLine(pro.GetName());

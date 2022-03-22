ef637282187d6c42dd14a47ef3e9f4a86f2e1e30
Giannis Eskioglou <johneski65@gmail.com>
2022-03-22 22:14:22 +0200

    first migration

--- Session-15/PetShopLibrary.EF/Context/PetShopContext.cs
+++ Session-15/PetShopLibrary.EF/Context/PetShopContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PetShopLibrary.EF.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,22 @@ namespace PetShopLibrary.EF
         public DbSet<Pet> Pets { get; set; }
         public DbSet<PetFood> PetFoods { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new PetConfiguration());
+            modelBuilder.ApplyConfiguration(new PetFoodConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        }
     }
 }
---
+++ Session-15/PetShopLibrary.EF/Migrations/20220322201347_InitialPetShopDb.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetShopLibrary.EF.Migrations
+{
+    public partial class InitialPetShopDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Tin = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ObjectStatus = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false),
+                    EmpType = table.Column<int>(type: "int", nullable: false),
+                    ObjectStatus = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PetFoods",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ObjectStatus = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false),
+                    Cost = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetFoods", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmployeeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PetID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PetPrice = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false),
+                    PetFoodID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PetFoodQty = table.Column<int>(type: "int", nullable: false),
+                    PetFoodPrice = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FoodTypeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HealthStatus = table.Column<int>(type: "int", nullable: false),
+                    AnimalType = table.Column<int>(type: "int", nullable: false),
+                    Breed = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ObjectStatus = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false),
+                    Cost = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Pets_PetFoods_FoodTypeID",
+                        column: x => x.FoodTypeID,
+                        principalTable: "PetFoods",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_FoodTypeID",
+                table: "Pets",
+                column: "FoodTypeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Pets");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "PetFoods");
+        }
+    }
+}

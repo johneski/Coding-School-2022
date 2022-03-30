4937b04bad473894e64147a11585b6a4df2057eb
Giannis Eskioglou <johneski65@gmail.com>
2022-03-30 16:10:03 +0300

    Exercise 3

--- Session-20/exercise.js
+++ Session-20/exercise.js
@@ -16,6 +16,43 @@ function IsPalindrome(e){
     }
     
     alert(`It is NOT a palindrome!!\nOriginal: ${value}\nReversed: ${reversed}`);
+}
 
+function ParseFormData(e){
+    let name = document.querySelector('#txtName').value;
+    let surname = document.querySelector('#txtSurname').value;
+    let age = document.querySelector('#txtAge').value;
+    let gender = document.querySelector('input[name="Gender"]:checked').value;
     
+    InsertToTable(Capitalize(name), Capitalize(surname), age, gender);
+}
+
+function Capitalize(s){
+    return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+}
+
+function InsertToTable(name, surname, age, gender){
+    let table = document.querySelector('#customerTable');
+    let tr = document.createElement('tr');
+    let nametd = document.createElement('td');
+    nametd.innerHTML = name;
+    tr.appendChild(nametd);
+    let surnametd = document.createElement('td');
+    surnametd.innerHTML = surname;
+    tr.appendChild(surnametd);
+    let agetd = document.createElement('td');
+    agetd.innerHTML = age;
+    tr.appendChild(agetd);
+    let gendertd = document.createElement('td');
+    gendertd.innerHTML = gender;
+    tr.appendChild(gendertd);
+    tr.addEventListener('click', SelectedRow);
+    table.appendChild(tr);
+}
+
+function SelectedRow(e){
+    document.querySelector('#txtNameDetails').value = this.children[0].innerText;
+    document.querySelector('#txtSurnameDetails').value = this.children[1].innerText;
+    document.querySelector('#txtAgeDetails').value = this.children[2].innerText;
+    document.querySelector('#txtGenderDetails').value = this.children[3].innerText;
 }
